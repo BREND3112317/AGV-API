2e7fbdde066cfc976cdea8bb5c0414c42f30424f
BREND3112317 <brend.main@gmail.com>
2020-11-22 21:35:34 +0800

    update for this-version AGV-View

--- src/API/http_test.php
+++ src/API/http_test.php
@@ -8,7 +8,8 @@ use BREND\AGV\Models\AGV_response;
 use BREND\AGV\Controllers\AGVController;
 
 //$post = new AGV_response(AGV_request::POST("ITRI_3-1", "500", array(), AGV_request::$url));
-$AGV1 = new AGVController("ITRI_3-1", "http://192.168.101.234:50100/AGV/Test");
+//$AGV1 = new AGVController("ITRI_3-1", "http://192.168.101.234:50100/AGV/Test");
+$AGV1 = new AGVController("ITRI_3-4");
 
 var_dump($AGV1->getStatus()->toArray());
 
