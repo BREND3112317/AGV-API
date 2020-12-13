56320c60c39210c4a8dee1e4db72da556ec78c68
BREND3112317 <brend.main@gmail.com>
2020-12-13 15:28:47 +0800

    completed pathsearch-AStar amd preparing pathsearch-DFS

--- src/API/PathSearch_1.php
+++ src/API/PathSearch_1.php
@@ -1,4 +1,6 @@
 <?php
+
+set_time_limit(1);
 $_map = [    [-1,-2, 0, 0,-1,-1,-1],
             [-1,-1, 0, 0, 0,-2,-2],
             [-1,-1, 0, 0, 0,-2,-2],
@@ -9,11 +11,15 @@ $_map = [    [-1,-2, 0, 0,-1,-1,-1],
 ];
 
 
-trait point{
-    public $x, $y;
-    public function __construct($x, $y){
-        $this->x = $x;
-        $this->y = $y;
+class point{
+    public $x, $y, $yaw;
+    public $cost;
+    public $parent;
+    public function __construct($x, $y, $yaw, $cost = PHP_INT_MAX-1){
+        $this->x    = $x;
+        $this->y    = $y;
+        $this->yaw  = $yaw;
+        $this->cost = $cost;
     }
 }
 
@@ -25,7 +31,8 @@ trait point{
     
 // }
 
-class SearchPath{
+class DFS{
+
     public $map = [ [-1,-2, 0, 0,-1,-1,-1],
                     [-1,-1, 0, 0, 0,-2,-2],
                     [-1,-1, 0, 0, 0,-2,-2],
@@ -35,82 +42,104 @@ class SearchPath{
                     [-1, 0, 0, 0, 0, 0,-1],
     ];
 
+    public $map_index = array();
+
+    public $best_path = array();
+    public $best_coin = PHP_INT_MAX;
+
+    // public $map = [ [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    //                 [-1,-1,-2, 0, 0,-1,-1,-1,-1],
+    //                 [-1,-1,-1, 0, 0, 0,-2,-2,-1],
+    //                 [-1,-1,-1, 0, 0, 0,-2,-2,-1],
+    //                 [-1,-1,-1, 0, 0, 0, 0,-2,-1],
+    //                 [-1,-1, 0, 0, 0, 0, 0,-2,-1],
+    //                 [-1,-1, 0, 0, 0, 0, 0,-1,-1],
+    //                 [-1,-1, 0, 0, 0, 0, 0,-1,-1],
+    //                 [-1,-1,-1,-1,-1,-1,-1,-1,-1],
+    // ];
+
     public $start, $end;
 
-    public $x_dis, $y_dis;
-
-    public $open_set, $close_set;
-
-    public function __construct($map){
-        $this->map = $map;
-    }
-
-    public function BaseCost($p){ // 節點道起點的移動代價，對應上文的g(n)
-        $this->x_dis = $p->x;
-        $this->y_dis = $p->y;
-        return $this->x_dis + $this->y_dis + (sqrt(2)-2)*min($this->x_dis, $this->y_dis);
-    }
-
-    public function HeuristicCost($p){ // 節點到終點的啟發函數，對應上文的h(n)。於是我們基於網格的圖形，所以這個函數和上一個函數用的對角距離
-        $this->x_dis = count($map) - 1 - $p->x;
-        $this->y_dis = count($map[0]) - 1 - $p->y;
-        return $this->x_dis + $this->y_dis + (sqrt(2)-2)*min($this->x_dis, $this->y_dis);
-    }
-
-    public function TotalCost($p){ // 代價總和，也是上面提到的f(n)
-        return $this->BaseCost($p) + $this->HeuristicCost($p);
-    }
-
-    public function IsValidPoint($p){ // 判斷點是否有效，不再地圖內或障礙物都是無效的
-        if($p->x < 0 || $p->y < 0)return false;
-        if($p->x >= count($map) || $p->y >= count($map[0]))return false;
-        return $map[$p->x-1][$p->y-1] !== 0;
-    }
-
-    public function IsInPointList($p, $point_list){ // 判斷點是否在某個集合中
-        foreach($point_list as $point){
-            if($p->x == $point->x && $p->y == $point->y)return true;
+    public function __construct($_map = null){
+        if($_map!==null){
+            $this->map = $_map;
         }
-        return false;
     }
 
-    public function IsInOpenList($p){ // 判斷點是否在open_set中
-        return IsInPointList($p, $this->open_set);
+    public function YawCost($x, $y, $p){ // 節點道起點的移動代價，對應上文的g(n)
+        $cost = 1;
+        $cx = $p->x-$x;
+        $cy = $p->y-$y;
+        if($cx==-1)$cost += 2;
+        else if($cy !=0 )$cost += 1;
+        return $cost;
     }
 
-    public function IsInCloseList($p){ // 判斷點是否在clost_set中
-        return IsInPointList($p, $this->close_set);
+    public function IsValidPoint($x, $y){ // 判斷點是否有效，不再地圖內或障礙物都是無效的
+        if($x < 0 || $y < 0)return false;
+        if($x >= count($this->map) || $y >= count($this->map[0]))return false;
+        //echo "(" . $x . ", " . "): " . $this->map[$x][$y] . '<br/>';
+        return $this->map[$y][$x] === 0;
+    }
+
+    public function setStartPoint($p){ // 設定起點
+        $this->start = new Point($p->x, $p->y, $p->yaw, 0);
     }
 
     public function IsStartPoint($p){ // 判斷點是否為起點
-        return $this->start->x == $p.x && $this->start->y == $p->y;
+        return $this->start->x == $p->x && $this->start->y == $p->y;
+    }
+
+    public function setEndPoint($p){ // 設定終點
+        $this->end = new Point($p->x, $p->y, $p->yaw);
     }
 
     public function IsEndPoint($p){ // 判斷點是否為終點
-        return $this->end->x == $p.x && $this->end->y == $p->y;
+        return $this->end->x == $p->x && $this->end->y == $p->y;
     }
 
-    public $width = 7, $height = 7;
-    public function showMap(){
-        for($i=0;$i<count($this->map);++$i){
-            for($j=0;$j<count($this->map[$i]);++$j){
-                echo $this->map[$i][$j] . ' ';
-            }
-            echo '<br />';
+    public function Run($start_p, $end_p){
+        $this->setStartPoint($start_p);
+        $this->setEndPoint($end_p);
+
+        $this->ProcessPoint($start_p->x,    $start_p->y+1,  $start_p);
+        $this->ProcessPoint($start_p->x-1,  $start_p->y,    $start_p);
+        $this->ProcessPoint($start_p->x,    $start_p->y-1,  $start_p);
+        $this->ProcessPoint($start_p->x+1,  $start_p->y,    $start_p);
+    }
+
+    public function ProcessPoint($x, $y, $parent){
+        $p = new point($x, $y, $parent->cost+1);
+        if($this->IsValidPoint($p->x, $p->y)==false) return;
+        if($this->IsEndPoint($p)){
+            $this->BuildPath($p);
+            return;
+        }
+        if(isset($this->map_index[$y][$x])==false || $this->map_index[$y][$x]->cost > $p->cost){
+            $this->map_index[$y][$x] = $p;
+            $this->ProcessPoint($p->x,      $p->y+1,    $p);
+            $this->ProcessPoint($p->x-1,    $p->y,      $p);
+            $this->ProcessPoint($p->x,      $p->y-1,    $p);
+            $this->ProcessPoint($p->x+1,    $p->y,      $p);
+        }
+        
+        //$map[$y][$x] = $this->YawCost($x, $y, $parent);
+    }
+
+    public function BuildPath($p){
+        $path = array();
+        $coin = 0;
+        while($p!=null){
+            array_push($path, $p);
+            $coin += $p->coin;
+            $p = $p->parent;
+        }
+        if($coin < $this->best_coin){
+            $this->best_coin = $coin;
+            $this->best_path = $path;
         }
     }
-
-    public function getPath($start_x, $start_y, $end_x, $end_y){
-        $this->showMap();
-        echo "(" . ($start_x) . ", " . ($start_y) . ")<br /><br />";
-        if(($start_x>=0 && $start_x<$this->width && $start_y>=0 && $start_y<$this->height) == false || $this->map[$start_x][$start_y]!=0)return ;
-        $index_x = $start_x<$end_x ? ($start_x==$end_x ? 0 : 1) : -1;
-        $index_y = $start_y<$end_y ? ($start_y==$end_y ? 0 : 1) : -1;
-        $this->map[$start_x][$start_y]++;
-        $this->getPath($start_x+$index_x, $start_y, $end_x, $end_y);
-        $this->getPath($start_x, $start_y+$index_y, $end_x, $end_y);
-        $this->map[$start_x][$start_y]++;
-    }
 }
-$searchPah = new SearchPath();
-$searchPah->getPath(4, 2, 5, 6);
+
+$bfs = new DFS();
+$bfs->Run(new point(4, 2), new point(5, 5));
